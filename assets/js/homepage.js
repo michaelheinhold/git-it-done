dd0908bd60020ee675ee703c20286b9307cd80d1
DerVerruckte <michael.heinhold@gmail.com>
2021-08-26 18:08:38 -0600

    language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -6,6 +6,8 @@ var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
 
+var languageButtonsEl =document.querySelector('#language-buttons');
+
 var getUserRepos = function(user) {
     //format github api url
     var apiUrl ="https://api.github.com/users/"+user+"/repos";
@@ -85,8 +87,32 @@ var displayRepos = function(repos, searchTerm) {
         //append to dom
         repoContainerEl.appendChild(repoEl);
     }
-}
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                displayRepos(data.items, language)
+            });
+        } else {
+            alert('Error: GitHub User Not Found');
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute('data-language');
+    console.log(language);
+    if(language){
+        getFeaturedRepos(language);
+        repoContainerEl.textContent="";
+    } 
+};
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener('click', buttonClickHandler);
 
 getUserRepos();
